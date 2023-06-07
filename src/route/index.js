a3ba4faddda785e49db1ff860e697fa1ff447a54
YM19 <127491550+YaroslavMariash@users.noreply.github.com>
2023-06-07 12:31:10 +0300

    my homework

--- src/route/index.js
+++ src/route/index.js
@@ -8,14 +8,15 @@ const router = express.Router()
 // router.get Створює нам один ентпоїнт
 
 //           ↙ тут вводимо шлях (PATH) до сторінки
-router.get('/', function (req, res) {
+router.get('/index', function (req, res) {
   // res.render генерує нам HTML сторінку
 
   //            ↙ cюди вводимо назву файлу з сontainer
-  res.render('index', {})
+  res.render('index', {
+    layout: 'default',
+  })
   //                  ↑↑ сюди вводимо JSON дані
 })
-
 // ================================================================
 
 // Підключаємо роутер до бек-енду
